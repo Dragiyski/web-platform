f27758804a2aeec21ccc5ef511665bf0bf1f553a
Plamen Dragiyski <plamen@dragiyski.org>
2022-11-10 22:09:03 +0100

    wip: context

--- v8-extension/src/context.cc
+++ v8-extension/src/context.cc
@@ -1,6 +1,24 @@
 #include "context.h"
 #include "js-helper.h"
 
+#include <mutex>
+#include <map>
+
+namespace {
+    std::map<v8::Isolate *, v8::Global<v8::FunctionTemplate>> template_map;
+    std::mutex template_map_mutex;
+    std::map<v8::Isolate *, v8::Global<v8::Private>> context_symbol_map;
+    std::mutex context_symol_map_mutex;
+
+    void v8_throw_illegal_constructor(const v8::FunctionCallbackInfo<v8::Value> &info) {
+        auto isolate = info.GetIsolate();
+        v8::HandleScope scope(isolate);
+        auto context = isolate->GetCurrentContext();
+
+        JS_THROW_ERROR(NOTHING, context, TypeError, "Illegal constructor");
+    }
+}
+
 void js_global_of(const v8::FunctionCallbackInfo<v8::Value> &info) {
     auto isolate = info.GetIsolate();
     v8::HandleScope scope(isolate);
@@ -11,29 +29,36 @@ void js_global_of(const v8::FunctionCallbackInfo<v8::Value> &info) {
     }
 }
 
-std::map<v8::Isolate *, v8::Global<v8::FunctionTemplate>> Context::class_template;
-
-void v8_throw_illegal_constructor(const v8::FunctionCallbackInfo<v8::Value> &info) {
-    auto isolate = info.GetIsolate();
-    v8::HandleScope scope(isolate);
-    auto context = isolate->GetCurrentContext();
-
-    JS_THROW_ERROR(NOTHING, context, TypeError, "Illegal constructor");
-}
-
 v8::MaybeLocal<v8::FunctionTemplate> Context::Template(v8::Local<v8::Context> context) {
     auto isolate = context->GetIsolate();
-    auto location = class_template.find(isolate);
-    if (location != class_template.end()) {
+    v8::EscapableHandleScope scope(isolate);
+    std::lock_guard template_map_lock(template_map_mutex);
+    auto location = template_map.find(isolate);
+    if (location != template_map.end()) {
         auto result = location->second.Get(isolate);
-        return result;
+        return scope.Escape(result);
     }
     auto tpl = v8::FunctionTemplate::New(isolate, Constructor, v8::Local<v8::Value>(), v8::Local<v8::Signature>());
     JS_EXECUTE_RETURN_HANDLE(JS_NOTHING(v8::FunctionTemplate), v8::String, name, ToString(context, "Context"));
     tpl->SetClassName(name);
     tpl->InstanceTemplate()->SetInternalFieldCount(1);
-    class_template.insert(std::make_pair(isolate, v8::Global<v8::FunctionTemplate>(isolate, tpl)));
-    return tpl;
+    template_map.insert(std::make_pair(isolate, v8::Global<v8::FunctionTemplate>(isolate, tpl)));
+    return scope.Escape(tpl);
+}
+
+v8::MaybeLocal<v8::Private> Context::Symbol(v8::Local<v8::Context> context) {
+    auto isolate = context->GetIsolate();
+    v8::EscapableHandleScope scope(isolate);
+    std::lock_guard symbol_lock(context_symol_map_mutex);
+    auto location = context_symbol_map.find(isolate);
+    if (location != context_symbol_map.end()) {
+        auto result = location->second.Get(isolate);
+        return scope.Escape(result);
+    }
+    JS_EXECUTE_RETURN_HANDLE(JS_NOTHING(v8::Private), v8::String, name, ToString(context, "Context"));
+    auto result = v8::Private::New(isolate, name);
+    context_symbol_map.insert(std::make_pair(isolate, v8::Global<v8::Private>(isolate, result)));
+    return scope.Escape(result);
 }
 
 void Context::Constructor(const v8::FunctionCallbackInfo<v8::Value> &info) {
@@ -78,17 +103,61 @@ void Context::Constructor(const v8::FunctionCallbackInfo<v8::Value> &info) {
         global_constructor_template->SetClassName(context_name);
     }
 
+    JS_EXECUTE_RETURN_HANDLE(NOTHING, v8::Private, context_symbol, Symbol(context));
+
+    auto task_queue = v8::MicrotaskQueue::New(isolate, v8::MicrotasksPolicy::kExplicit);
+
     auto new_context = v8::Context::New(
         isolate,
         nullptr,
         global_constructor_template->InstanceTemplate(),
         v8::MaybeLocal<v8::Value>(),
         v8::DeserializeInternalFieldsCallback(),
-        context->GetMicrotaskQueue()
+        task_queue.get()
     );
 
-    auto promise_init_hook = v8::Set::New(isolate);
-    auto promise_before_hook = v8::Set::New(isolate);
-    auto promise_after_hook = v8::Set::New(isolate);
-    auto promise_resolve_hook = v8::Set::New(isolate);
+    new_context->AllowCodeGenerationFromStrings(true);
+    new_context->Global()->SetPrivate(context, context_symbol, holder);
+    auto wrapper = new Context(context, new_context, std::move(task_queue));
+    wrapper->Wrap(holder);
+
+    context->GetMicrotaskQueue()->AddMicrotasksCompletedCallback(creation_context_after_microtasks_completed);
+
+    info.GetReturnValue().Set(info.This());
+}
+
+Context::Context(
+    v8::Local<v8::Context> control_context,
+    v8::Local<v8::Context> wrap_context,
+    std::unique_ptr<v8::MicrotaskQueue> &&task_queue
+) : m_isolate(control_context->GetIsolate()),
+m_control_context(control_context->GetIsolate(), control_context),
+m_context(control_context->GetIsolate(), wrap_context),
+m_task_queue(std::move(task_queue)) {}
+
+Context::~Context() {
+    if (!m_isolate->IsDead()) {
+        auto control_context = m_control_context.Get(m_isolate);
+        auto microtask_queue = control_context->GetMicrotaskQueue();
+        if (microtask_queue != nullptr) {
+            microtask_queue->RemoveMicrotasksCompletedCallback(creation_context_after_microtasks_completed);
+        }
+    }
+}
+
+void Context::creation_context_after_microtasks_completed(v8::Isolate *isolate, void *ptr_wrapper) {
+    auto wrapper = reinterpret_cast<Context *>(ptr_wrapper);
+    if (!wrapper->auto_run_microtask_queue) {
+        return;
+    }
+    auto context = wrapper->m_context.Get(isolate);
+    context->GetMicrotaskQueue()->PerformCheckpoint(isolate);
+}
+
+void Context::NativeFunction(const v8::FunctionCallbackInfo<v8::Value> &info) {
+    auto isolate = info.GetIsolate();
+    v8::HandleScope scope(isolate);
+    v8::Local<v8::Context> context = isolate->GetCurrentContext();
+
+    // Create a function that calls v8 API function, which should declare to context it is not running any user code.
 }
--- v8-extension/src/context.h
+++ v8-extension/src/context.h
@@ -2,28 +2,36 @@
 #define V8EXT_CONTEXT_H
 
 #include <v8.h>
+#include <node_object_wrap.h>
 #include <map>
+#include <mutex>
+#include <condition_variable>
 #include <thread>
+#include <optional>
+#include <chrono>
 
 void js_global_of(const v8::FunctionCallbackInfo<v8::Value> &info);
 
-void js_create_context(const v8::FunctionCallbackInfo<v8::Value> &info);
-
-class Context {
-private:
-    static std::map<v8::Isolate *, v8::Global<v8::FunctionTemplate>> class_template;
+class Context : node::ObjectWrap {
 public:
     static v8::MaybeLocal<v8::FunctionTemplate> Template(v8::Local<v8::Context> context);
+    static v8::MaybeLocal<v8::Private> Symbol(v8::Local<v8::Context> context);
 protected:
     static void Constructor(const v8::FunctionCallbackInfo<v8::Value> &info);
-    static void callback_promise_init_hook(const v8::FunctionCallbackInfo<v8::Value> &info);
-    static void callback_promise_before_hook(const v8::FunctionCallbackInfo<v8::Value> &info);
-    static void callback_promise_after_hook(const v8::FunctionCallbackInfo<v8::Value> &info);
-    static void callback_promise_resolve_hook(const v8::FunctionCallbackInfo<v8::Value> &info);
+    static void NativeFunction(const v8::FunctionCallbackInfo<v8::Value> &info);
+    static void creation_context_after_microtasks_completed(v8::Isolate *, void *);
 protected:
+    v8::Isolate *m_isolate;
     v8::Persistent<v8::Context> m_context;
-    v8::Persistent<v8::Set> m_promise_init_hook, m_promise_before_hook, m_promise_after_hook, m_promise_resolve_hook;
-    // std::unique_ptr<std::jthread>
+    v8::Persistent<v8::Context> m_control_context;
+    std::unique_ptr<v8::MicrotaskQueue> m_task_queue;
+    bool auto_run_microtask_queue = false;
+    std::optional<std::chrono::steady_clock::duration> m_max_sync_time;
+    std::optional<std::chrono::steady_clock::duration> m_max_user_time;
+protected:
+    explicit Context(v8::Local<v8::Context> control_context, v8::Local<v8::Context> wrap_context, std::unique_ptr<v8::MicrotaskQueue> &&task_queue);
+public:
+    ~Context();
 };
 
 #endif /* V8EXT_CONTEXT_H */
